fa638af4bddb414036e793024143accaf4a5d515
Bathri Vijay <65590714+bathrivijay05@users.noreply.github.com>
2021-04-11 11:46:11 +0530

    Update scripts.js

--- jsapi/scripts.js
+++ jsapi/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://api.rootnet.in/covid19-in/stats/latest', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
